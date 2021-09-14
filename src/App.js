aaa3a6426150bb1d44df3affa87fe3a78d46498a
Xuan Huan Ninh <huannx@mintpotvn.com>
2021-09-14 23:15:24 +0700

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import React from "react";
+import Login from "./components/Login";
+import RoomList from "./components/RoomList";
+import ChatRoom from "./components/ChatRoom";
+import AddRoom from "./components/AddRoom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" component={Login}></Route>
+                <Route path="/room-list" component={RoomList}></Route>
+                <Route path="/chat-room" component={ChatRoom}></Route>
+                <Route path="/add-room" component={AddRoom}></Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,34 @@
+/*
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBwSLI9PAXLbcWo2xbV6UOMLy6Npx-Sg1U",
+    authDomain: "reactchat-1757f.firebaseapp.com",
+    projectId: "reactchat-1757f",
+    storageBucket: "reactchat-1757f.appspot.com",
+    messagingSenderId: "214293260411",
+    appId: "1:214293260411:web:34d495502f3cc9e6d65ade"
+})
+
+export default firebase;*/
+
+
+import firebase from "firebase";
+
+const settings = {timestampsInSnapshots: true};
+const config = {
+    apiKey: "AIzaSyBwSLI9PAXLbcWo2xbV6UOMLy6Npx-Sg1U",
+    authDomain: "reactchat-1757f.firebaseapp.com",
+    projectId: "reactchat-1757f",
+    storageBucket: "reactchat-1757f.appspot.com",
+    messagingSenderId: "214293260411",
+    appId: "1:214293260411:web:34d495502f3cc9e6d65ade"
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
